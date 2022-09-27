e67da50341da60330de7a0c51014c4c4c06330b4
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-26 23:02:27 -0300

    Criado os serviços responsáveis por atualizar e deletar uma tarefa

--- src/main/java/com/br/ayrton/todolist/service/TaskService.java
+++ src/main/java/com/br/ayrton/todolist/service/TaskService.java
@@ -2,13 +2,11 @@ package com.br.ayrton.todolist.service;
 
 import com.br.ayrton.todolist.model.Task;
 import com.br.ayrton.todolist.repositoy.TaskRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -27,5 +25,24 @@ public class TaskService {
         return taskRepository.findById(id)
                 .map(task -> ResponseEntity.ok().body(task))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Object> deleteTaskById(Long id){
+        return taskRepository.findById(id)
+                .map(taskToDelete ->{
+                    taskRepository.deleteById(id);
+                    return ResponseEntity.noContent().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Task> updateTaskById(Task task, Long id){
+        return taskRepository.findById(id)
+                .map(taskToUpdate ->{
+                 taskToUpdate.setTitle(task.getTitle());
+                 taskToUpdate.setDescription(taskToUpdate.getDescription());
+                 taskToUpdate.setDeadLine(taskToUpdate.getDeadLine());
+                 Task updated = taskRepository.save(task);
+                 return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
