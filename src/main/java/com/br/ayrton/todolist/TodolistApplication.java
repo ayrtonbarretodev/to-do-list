ceecc82c42add126f0aedecdc465db634b1099ce
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-27 11:01:18 -0300

    Configurando informações da API através da anotações OpenAPIDefinition

--- src/main/java/com/br/ayrton/todolist/TodolistApplication.java
+++ src/main/java/com/br/ayrton/todolist/TodolistApplication.java
@@ -1,9 +1,12 @@
 package com.br.ayrton.todolist;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API de tarefas", version = "1.0", description = "Informações de tarefas"))
 public class TodolistApplication {
 
     public static void main(String[] args) {
