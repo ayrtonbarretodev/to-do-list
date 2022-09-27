144bbfe2a4626835fbcc2523a0515d57ac8cf9be
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-26 23:14:18 -0300

    adicionando a tag @AllArgsConstructor e removendo a tag Autowired, e adicionando o Slf4j para logs nos métodos do controller

--- src/main/java/com/br/ayrton/todolist/controller/TaskController.java
+++ src/main/java/com/br/ayrton/todolist/controller/TaskController.java
@@ -2,46 +2,53 @@ package com.br.ayrton.todolist.controller;
 
 import com.br.ayrton.todolist.model.Task;
 import com.br.ayrton.todolist.service.TaskService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
+@Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
 public class TaskController {
 
-    @Autowired
     private TaskService taskService;
 
     @PostMapping("/tasks")
     public ResponseEntity<Task> createTask (@RequestBody Task task){
+        log.info("Criando uma nova tarefa com as informações [{}]", task);
         return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(task));
     }
 
     @GetMapping("/tasks")
     public ResponseEntity<List<Task>> getAllTasks(){
+        log.info("Listando todas as tarefas cadastradas");
         return ResponseEntity.status(HttpStatus.OK).body(taskService.listAllTasks());
     }
 
     @GetMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> getTaskById(@PathVariable (value = "id") Long id){
+        log.info("Buscando tarefa com o id [{}]", id);
         return taskService.findTaskById(id);
     }
 
     @PutMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> updateTaskById(@PathVariable (value = "id") Long id, @RequestBody Task task){
+        log.info("Atualizando a tarefa com o id [{}] as novas informações são: [{}]",id,task);
         return taskService.updateTaskById(task,id);
     }
 
     @DeleteMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Object> deleteTaskById(@PathVariable (value = "id") Long id){
+        log.info("Excluindo tarefas com o id [{}]", id);
         return taskService.deleteTaskById(id);
     }
 }
