245488fbd79f88139868e440611e46babee05b70
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-27 11:51:25 -0300

    Através da anotação Operation, informando o nome e uma descrição do endpoint para que apareça na documentação do Swagger e os erros e suas determinadas descrições

--- src/main/java/com/br/ayrton/todolist/controller/TaskController.java
+++ src/main/java/com/br/ayrton/todolist/controller/TaskController.java
@@ -2,6 +2,9 @@ package com.br.ayrton.todolist.controller;
 
 import com.br.ayrton.todolist.model.Task;
 import com.br.ayrton.todolist.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -18,17 +21,33 @@ public class TaskController {
 
     private TaskService taskService;
 
+
+    @Operation(summary = "Create Task",description = "Criando uma nova tarefa",responses = {
+            @ApiResponse(responseCode = "201", description = "Tarefa criada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Houve um erro ao criar a tarefa, verifique as informações")
+    })
+
     @PostMapping("/tasks")
     public ResponseEntity<Task> createTask (@RequestBody Task task){
         log.info("Criando uma nova tarefa com as informações [{}]", task);
         return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(task));
     }
 
+    @Operation(summary = "List Task",description = "Listando todas as tarefas",responses = {
+            @ApiResponse(responseCode = "200", description = "Tarefas listadas com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Houve um erro ao listar as tarefas")
+    })
+
     @GetMapping("/tasks")
     public ResponseEntity<List<Task>> getAllTasks(){
         log.info("Listando todas as tarefas cadastradas");
         return ResponseEntity.status(HttpStatus.OK).body(taskService.listAllTasks());
     }
+
+    @Operation(summary = "List Task By Id",description = "Buscando uma tarefa por id", responses = {
+            @ApiResponse(responseCode = "200", description = "Tarefa encontrada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrada nenhuma tarefa com esse id")
+    })
 
     @GetMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -37,12 +56,22 @@ public class TaskController {
         return taskService.findTaskById(id);
     }
 
+    @Operation(summary = "Update Task",description = "Atualizando uma tarefa",responses = {
+            @ApiResponse(responseCode = "200", description = "Tarefa atualizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não foi possível atualizar a tarefa - tarefa não encontrada")
+    })
+
     @PutMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> updateTaskById(@PathVariable (value = "id") Long id, @RequestBody Task task){
         log.info("Atualizando a tarefa com o id [{}] as novas informações são: [{}]",id,task);
         return taskService.updateTaskById(task,id);
     }
+
+    @Operation(summary = "Delete Task",description = "Deletando uma tarefa", responses = {
+            @ApiResponse(responseCode = "204", description = "Tarefa excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não foi possível excluir a tarefa - tarefa não encontrada")
+    })
 
     @DeleteMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
