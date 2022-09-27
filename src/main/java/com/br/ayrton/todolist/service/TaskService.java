a97e50352d93c89ce09643a76b2a94c22943f226
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-26 21:02:47 -0300

    Criado os serviços de buscar todas as tarefas e buscar as tarefas por id

--- src/main/java/com/br/ayrton/todolist/service/TaskService.java
+++ src/main/java/com/br/ayrton/todolist/service/TaskService.java
@@ -2,8 +2,13 @@ package com.br.ayrton.todolist.service;
 
 import com.br.ayrton.todolist.model.Task;
 import com.br.ayrton.todolist.repositoy.TaskRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -12,5 +17,15 @@ public class TaskService {
 
     public Task createTask (Task task){
         return taskRepository.save(task);
+    }
+
+    public List<Task> listAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> findTaskById(Long id){
+        return taskRepository.findById(id)
+                .map(task -> ResponseEntity.ok().body(task))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
